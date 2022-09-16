460c2f75705a25826ec7112f17f27c114a0d4c43
Walkerjony <vctornascimento8@gmail.com>
2022-09-16 06:43:13 -0300

    second commit

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,9 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('cadastraProduto')" :active="request()->routeIs('dashboard')">
+                        {{ __('Cadastrar') }}
+                    </x-nav-link>
                 </div>
             </div>
 
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\produtoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,3 +23,5 @@ Route::get('/dashboard', function () {
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
+
+Route::get('/cadastra', [produtoController::class, 'cadastra'])->name('cadastraProduto');
